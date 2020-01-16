0396dcc993eb550706e65b584d84df987417967f
Ursula MorenoVanderLaan <ursula.moreno-vanderlaan@techtonic.com>
2020-01-16 13:32:52 -0700

    Working with intersecting inputs

--- server.py
+++ server.py
@@ -14,9 +14,25 @@ def geosect():
     # get form data
     data = request.get_json()
     coords =  list(data.values())
+    polygons = []
 
+    for item in coords:
+        #check we have a polygon
+        geometry = item['geometry']
+        if geometry['type'] != 'Polygon':
+            return 'Please use a valid polygon'
 
-    return data
+        #create list of polygon coordinates
+        coordinates = geometry['coordinates']
+        polygons.append(Polygon(coordinates[0]))
+    
+    #check if polygons intersect
+    intersect = polygons[0].intersects(polygons[1])
+    
+    if intersect == True:
+        return 'These polygons intersect!'
+    else:
+        return 'These polygons do not intersect.'
 
 if __name__ == '__main__':
     app.run()
